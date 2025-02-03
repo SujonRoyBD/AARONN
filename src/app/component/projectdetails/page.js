f4dae7986c464dbe4847fc17fab88514715e5314
SujonRoyBD <sujonroy537527@gmail.com>
2025-02-03 23:11:06 +0600

    image

--- src/app/component/projectdetails/page.js
+++ src/app/component/projectdetails/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Projectdetails = () => {
     return (
-        <div className='px-[150px]'>
+        <div className='px-5 md:px-[150px]'>
             <h3 className="text-3xl font-bold mt-5">Project Detail</h3>
             <p className='text-gray-500 mt-3 '>Details About The Porject</p>
             <hr className='w-full h-3 mt-9'/>
@@ -15,8 +15,15 @@ const Projectdetails = () => {
                 <p>The fact that photography has different meanings to different people is one of the many components of its appeal. Photography is such an important part of our life that it is now very difficult to imagine the world without it. </p>
                 <p className='mt-9'>We cannot imagine a wedding without the opportunity to capture it on film, we would not be able to remember the growing up of children or the holidays if we did not have pictures.</p>
             </div>
-            <div>
-                <Image src="/aseats/projectdetails/dryone.png"/>
+            <div className='grid grid-cols-1 md:grid-cols-2 mt-9 gap-7'>
+                <Image src="/aseats/projectdetails/dryone.png"alt='dry' width={555} height={600}/>
+                <Image src="/aseats/projectdetails/drytwo.png"alt='dry' width={555} height={600}/>
+                
+            </div>
+            <div className='text-gray-500 md:ml-[200px] mt-9'>
+                <h3 className='text-2xl font-bold'>Day One</h3>
+                <p>Everywhere we are haunted by photography in newspapers, magazines, advertisements on television on the Internet, but we still crave even more. </p>
+                <p className='mt-9'>And what helps to achieve a good result? We will look at these issues and some of the possibilities of photography and explain that it is a combination of thought imagination, visual design, technical skills and organizational skills</p>
             </div>
         </div>
     );
