59d5e490752575f3bd0b427254dbed5ebf8ef90a
SujonRoyBD <sujonroy537527@gmail.com>
2025-02-03 07:45:05 +0600

    rounded

--- src/app/component/lets/page.js
+++ src/app/component/lets/page.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Lets = () => {
     return (
         <div className='px-8 md:px-[150px] mt-16'>
-            <div className='grid grid-cols-1 md:grid-cols-5'>
+            <div className='grid grid-cols-1 md:grid-cols-5 gap-7'>
             <div className='md:col-span-3 mt-[70px]'>
                     <h3 className='text-3xl font-bold'>Lets get know <br /> about me closer</h3>
                     <p className='text-gray-400'>
@@ -13,6 +13,7 @@ const Lets = () => {
                 </div>
                 <div className='md:col-span-2 relative'>
                     <div className='border border-orange-500 h-8 w-28 rounded-full absolute right-[86px] -top-4'/>
+                    <div className='border  border-orange-500 h-[120px] w-[35px] rounded-full absolute -ml-[16px] mt-[100px]'></div>
                     <Image src="/aseats/lets-go/letus1.png" alt='man' height={250} width={300} className='border border-gray-950 rounded-lg bg-black'/>
                 </div>
             </div>
