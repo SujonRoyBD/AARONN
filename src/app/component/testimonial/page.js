fa80e6ac03d91b988ed282d828a7c21eb859ffea
SujonRoyBD <sujonroy537527@gmail.com>
2025-04-26 21:26:30 +0600

    text

--- src/app/component/testimonial/page.js
+++ src/app/component/testimonial/page.js
@@ -7,7 +7,7 @@ const Testimonial = () => {
           <div className='md:w-[700px] mt-9'>
           <h2 className='text-2xl font-bold '>Testimonial</h2>
             <p className='text-gray-400'>
-                “Aaronn was fantastic to work with from start to finish. We were looking for a simple, stand-out logo and he delivered. I tried designing the logo myself thinking I wouldn’t need to pay the money for a professional graphic designer but I was very wrong. Working with Aaronn was worth every penny and was surprisingly affordable! I remember him saying simplicity is key to a successful logo and boy he was right. I can’t thank Aaronn enough for his effort and professionalism. I would recommend him to anyone looking for a design!”
+                “Aaronn was fantastic to work with from start to finish. We were looking for a simple, stand-out logo and he delivered. I tried designing the logo myself thinking I wouldn't need to pay the money for a professional graphic designer but I was very wrong. Working with Aaronn was worth every penny and was surprisingly affordable! I remember him saying simplicity is key to a successful logo and boy he was right. I can't thank Aaronn enough for his effort and professionalism. I would recommend him to anyone looking for a design!”
                 -Martin lee</p>
           </div>
           <div className='md:ml-[300px]'>
